742969b897b8315bcc2fe41e9dd5139d532e6e46
Slawomir <slawomirsmiarowski@gmail.com>
2021-01-29 12:27:32 +0100

    code cleaning

--- istqb_glossary_2_anki.py
+++ istqb_glossary_2_anki.py
@@ -1,40 +1,37 @@
-import webbrowser, sys, pyperclip, os, pprint, logging, genanki
+import webbrowser, sys, logging, genanki, os, pyperclip, pprint
 from selenium import webdriver
 from selenium.webdriver.support.ui import Select
 logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
 logging.disable(logging.DEBUG)
-
-clear = lambda: os.system('cls') # utowrzenie metody clear w consoli
 
 browser = webdriver.Chrome()
 browser.get('https://glossary.istqb.org/pl/search') # go to the ISTQB Glossary page
 browser.find_element_by_css_selector('#menu-space > div > form > div.advance-search').click() # advance search options
 browser.find_element_by_css_selector('#advance-search-area > div > div.col-md-6.syllabi > span:nth-child(4)').click() # deselect all
 browser.find_element_by_css_selector('#advance-search-area > div > div.col-md-6.syllabi > label:nth-child(7) > input[type=checkbox]').click() # select ISTQB Foundation V3.1 2018
-browser.find_element_by_css_selector('#advance-search-area > div > div.col-md-6.methods > input[type=checkbox]:nth-child(19)').click() #  Multi Language Display
+browser.find_element_by_css_selector('#advance-search-area > div > div.col-md-6.methods > input[type=checkbox]:nth-child(19)').click() #  multi language display
 language_select = Select(browser.find_element_by_css_selector('#advance-search-area > div > div.col-md-6.methods > div:nth-child(21) > select:nth-child(1)'))  # Documentation https://selenium-python.readthedocs.io/navigating.html
-language_select.select_by_value("48") # Chose Polish as a secondary language
+language_select.select_by_value("48") # Polish as a secondary language
 browser.find_element_by_css_selector('#advance-search-area > div > div.col-md-6.methods > span.advance-search-btn.pull-right').click()
-browser.implicitly_wait(3) # seconds https://selenium-python.readthedocs.io/waits.html
+browser.implicitly_wait(3) # time in seconds https://selenium-python.readthedocs.io/waits.html
 
 glossary = []
 
 for j in range(1,13): # 13
     for i in range(3,23): # 23
         try:
-            glossary.append({ # stworzenie listy slownikow
+            glossary.append({ # list of dictionaries creation 
                 'term_eng': browser.find_element_by_css_selector(f"#app > div > div:nth-child(4) > div > div.search-results > div:nth-child({i}) > div.term-row > div > div.term-heading").text,
                 'desc_eng': "".join(browser.find_element_by_css_selector(f"#app > div > div:nth-child(4) > div > div.search-results > div:nth-child({i}) > div.term-row > div > div.term-definition-preview").text),
                 'term_pl': browser.find_element_by_css_selector(f"#app > div > div:nth-child(4) > div > div.search-results > div:nth-child({i}) > div:nth-child(2) > div > div > div.term-heading").text,
                 'desc_pl': browser.find_element_by_css_selector(f"#app > div > div:nth-child(4) > div > div.search-results > div:nth-child({i}) > div:nth-child(2) > div > div > div.term-definition-preview").text.replace('\n\t',' ')
             })
         except: logging.info('element has not been found')
-        logging.info("strona(j): " + str(j) + ", term(i): " + str(i))
+        logging.info("page(j): " + str(j) + ", term(i): " + str(i))
     try:
         browser.find_element_by_css_selector(f"span[data-page='{j+1}']").click()
     except:
-        logging.info("there are no more pages to click")
-    # if j==1: browser.implicitly_wait(3)
+        logging.info("there are no more pages to click"
     browser.implicitly_wait(3)
 
 
@@ -61,10 +58,9 @@ genanki_deck_istqb = genanki.Deck(
 
 
 for i in glossary:
-    # logging.info("\n" + i['term_eng'] + "/" + i['term_pl'] + "\n" + i['desc_eng'] + " \n" + i['desc_pl'])
     field1 = i['term_eng']
     field2 = i['term_pl']
-    field3 = i['desc_eng'] # + " | " + i['desc_pl']
+    field3 = i['desc_eng']
     field4 = i['desc_pl']
     my_note = genanki.Note(
     model=genanki_model_istqb,
@@ -75,24 +71,5 @@ for i in glossary:
 genanki.Package(genanki_deck_istqb).write_to_file('istqb_glossary.apkg')
 
 
-
-
-
-# dataList = [{'a': 1}, {'b': 3}, {'c': 5}]
-# for index in range(len(dataList)):
-#     for key in dataList[index]:
-#         print(dataList[index][key])
-
-
-
-# line = line.replace('\n','')
 browser.quit()
-# clear() # czyszczenie consoli
 # pprint.pprint(glossary)
-print(str(len(glossary)) + 'results')
-
-
-### Known issues:
-### -script is finding only 237-238 terms from avaliable 239
-### -there are unnecesery new line escape sequences in desc_eng/desc_pl
-### -waits should be changed
